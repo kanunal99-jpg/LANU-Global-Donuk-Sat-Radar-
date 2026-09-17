4af71e1915cfb7c0c4f77cca320e2f298b122af7
kanunal99-jpg <kanunal99@gmail.com>
2026-09-18 00:19:32 +0300

    test: add coroutine test dependency for sync engine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
     testImplementation("org.json:json:20260814")
 }
 
