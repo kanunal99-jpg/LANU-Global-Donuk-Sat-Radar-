9249fb79f5a951a148de56c7423d6323687240e1
kanunal99-jpg <kanunal99@gmail.com>
2026-09-19 10:09:34 +0300

    test: add missing compose semantics imports

--- app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
@@ -2,6 +2,9 @@ package com.lanu.globaldonuksatisradari
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performScrollToNode
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
