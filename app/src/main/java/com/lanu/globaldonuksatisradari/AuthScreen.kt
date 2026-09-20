31706aa521f5a61b833866b333a160b6596e4432
kanunal99-jpg <kanunal99@gmail.com>
2026-09-20 15:01:01 +0300

    fix: import auth card row layout

--- app/src/main/java/com/lanu/globaldonuksatisradari/AuthScreen.kt
+++ app/src/main/java/com/lanu/globaldonuksatisradari/AuthScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
