dec67eea667214c59b95af2014003d3d38a187fb
kanunal99-jpg <kanunal99@gmail.com>
2026-09-21 18:35:22 +0300

    test: cover Overpass inventory query coverage

---
+++ app/src/test/java/com/lanu/globaldonuksatisradari/data/OverpassBusinessSourceAdapterTest.kt
@@ -0,0 +1,32 @@
+package com.lanu.globaldonuksatisradari.data
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class OverpassBusinessSourceAdapterTest {
+    @Test
+    fun blankIstanbulQueryBuildsBroadBusinessInventory() {
+        val query = OverpassQueryBuilder.build("İstanbul", null, "")
+        assertTrue(query.contains("""nwr["name"]["amenity"]"""))
+        assertTrue(query.contains("""nwr["name"]["shop"]"""))
+        assertTrue(query.contains("""nwr["name"]["craft"]"""))
+    }
+
+    @Test
+    fun districtQueryUsesDistrictAdministrativeBoundary() {
+        val query = OverpassQueryBuilder.build("İstanbul", "Kadıköy", "")
+        assertTrue(query.contains("""["admin_level"="6"]"""))
+        assertTrue(query.contains("""["name"="Kadıköy"]"""))
+        assertTrue(query.contains("districtRelation"))
+    }
+
+    @Test
+    fun categoryQueriesCoverRequestedBusinessTypes() {
+        val cafe = OverpassQueryBuilder.build("İstanbul", "Pendik", "cafe")
+        val playstation = OverpassQueryBuilder.build("İstanbul", "Pendik", "PlayStation")
+        val cigkofte = OverpassQueryBuilder.build("İstanbul", "Pendik", "çiğköfte")
+        assertTrue(cafe.contains("""["amenity"="cafe"]"""))
+        assertTrue(playstation.contains("""["amenity"="internet_cafe"]"""))
+        assertTrue(cigkofte.contains("""["cuisine"~"cig_kofte|çiğ_köfte"""))
+    }
+}
