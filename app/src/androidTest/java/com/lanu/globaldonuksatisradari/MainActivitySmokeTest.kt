27c8f56abf73683ee15167da9820e39ca8e9252a
kanunal99-jpg <kanunal99@gmail.com>
2026-09-23 16:05:25 +0300

    test: scope product catalog inputs to dialog semantics

--- app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
@@ -203,9 +203,10 @@ class MainActivitySmokeTest {
         waitForText("Ürün kataloğu").performClick()
         waitForText("Ürün Kataloğu").assertIsDisplayed()
         composeRule.onNodeWithTag("product_add_surface").assertIsDisplayed().performClick()
-        waitForTag("product_name_input").performTextInput("Smoke Donuk Ürün")
-        waitForTag("product_price_input").performTextInput("125,50")
-        waitForTag("product_save_button").performClick()
+        waitForDialog().assertExists()
+        waitForDialogTag("product_name_input").performTextInput("Smoke Donuk Ürün")
+        waitForDialogTag("product_price_input").performTextInput("125,50")
+        waitForDialogTag("product_save_button").performClick()
         waitForText("Smoke Donuk Ürün").assertExists()
         waitForText("125,50 TRY").assertExists()
     }
