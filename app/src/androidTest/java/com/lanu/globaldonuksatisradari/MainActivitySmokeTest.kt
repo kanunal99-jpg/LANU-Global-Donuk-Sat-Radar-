a823b435fddbc060f6fc5faf3c4c8af5a101cd3f
kanunal99-jpg <kanunal99@gmail.com>
2026-09-23 14:01:21 +0300

    test: invoke explicit product add button semantics

--- app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
@@ -202,7 +202,7 @@ class MainActivitySmokeTest {
     fun productCatalog_canOpenAndAddManualPrice() {
         waitForText("Ürün kataloğu").performClick()
         waitForText("Ürün Kataloğu").assertIsDisplayed()
-        waitForText("Yeni ürün").assertIsDisplayed().performTouchInput { click(center) }
+        composeRule.onNodeWithTag("product_add_button").assertIsDisplayed().performClick()
         waitForTag("product_editor_open_state").assertExists()
         waitForDialog().assertExists()
         waitForDialogTag("product_name_input").performTextInput("Smoke Donuk Ürün")
