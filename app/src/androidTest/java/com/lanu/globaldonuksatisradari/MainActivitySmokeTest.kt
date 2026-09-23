91df3b06d74bb15c973228616c2f9a771c67ff2a
kanunal99-jpg <kanunal99@gmail.com>
2026-09-23 13:44:53 +0300

    test: physically tap visible product add label

--- app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
@@ -202,7 +202,7 @@ class MainActivitySmokeTest {
     fun productCatalog_canOpenAndAddManualPrice() {
         waitForText("Ürün kataloğu").performClick()
         waitForText("Ürün Kataloğu").assertIsDisplayed()
-        waitForText("Yeni ürün").assertIsDisplayed().performClick()
+        waitForText("Yeni ürün").assertIsDisplayed().performTouchInput { click(center) }
         waitForTag("product_editor_open_state").assertExists()
         waitForDialog().assertExists()
         waitForDialogTag("product_name_input").performTextInput("Smoke Donuk Ürün")
