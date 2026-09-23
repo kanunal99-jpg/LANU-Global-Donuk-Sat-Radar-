d4e6127fe136b0cb2b59a0ad616840a9d47a11b3
kanunal99-jpg <kanunal99@gmail.com>
2026-09-23 13:34:30 +0300

    test: trigger product editor through visible button text

--- app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/lanu/globaldonuksatisradari/MainActivitySmokeTest.kt
@@ -202,9 +202,7 @@ class MainActivitySmokeTest {
     fun productCatalog_canOpenAndAddManualPrice() {
         waitForText("Ürün kataloğu").performClick()
         waitForText("Ürün Kataloğu").assertIsDisplayed()
-        composeRule.onNodeWithTag("product_add_button")
-            .assertIsDisplayed()
-            .performTouchInput { click(center) }
+        waitForText("Yeni ürün").assertIsDisplayed().performClick()
         waitForTag("product_editor_open_state").assertExists()
         waitForDialog().assertExists()
         waitForDialogTag("product_name_input").performTextInput("Smoke Donuk Ürün")
